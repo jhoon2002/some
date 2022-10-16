f44c1a23ca8b39fc46a999bd72e1e89f5e5e21c8
Yeo <jhoon2002@naver.com>
2022-10-16 21:39:47 +0900

    notebook

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo  '<<bash 시작>>'  &&  echo
+installDependencies() {
+    echo  "nestjs/cli, quasar/cli 설치..."  && echo
+    npm i -g @nestjs/cli
+    npm i -g @quasar/cli
+}
+setupServer() {
+    cd ./backend || return
+    echo  "백엔드 빌드..."
+    yarn build
+    cd ..
+}
+setupClient() {
+    cd ./frontend || return
+    echo  "프론트엔드 빌드..."
+    yarn build
+    cd ..
+}
+#copy() {
+#    echo  " Copying assets to server"  &&  echo
+#    mkdir -p ./server/views
+#    cp -R ./client/dist/spa/* ./server/views
+#    }
+#}
+#installDependencies && setupServer && setupClient && copy
+#installDependencies && setupServer && setupClient && copy
+installDependencies && setupServer && setupClient
+echo  '<<bash 종료>>'  &&  echo
