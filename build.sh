25ed0b46a07317d3373a870ad637a84ab989e1bf
Yeo <jhoon2002@naver.com>
2022-10-16 22:51:28 +0900

    notebook

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 echo && echo && ehco  '<<<<<<<< bash 시작 >>>>>>>>>>'  &&  echo
 installDependencies() {
-    echo && echo  "[nestjs/cli, quasar/cli 설치...]"  && echo
-    npm i -g @nestjs/cli
-    npm i -g @quasar/cli
+    echo && echo && echo && echo  "[nestjs/cli, quasar/cli 설치...]"  && echo
+    yarn global add @nestjs/cli
+    yarn global add @quasar/cli
 }
 setupServer() {
     cd ./backend || return
-    echo && echo  "[백엔드 설치...]" && echo
-    yarn install
-    echo && echo  "[백엔드 빌드...]" && echo
+    #    echo && echo  "[백엔드 설치...]" && echo
+    #    yarn install
+     echo && echo && echo && echo  "[백엔드 빌드...]" && echo
     yarn build
     cd ..
 }
 setupClient() {
     cd ./frontend || return
-    echo && echo  "[프론트엔드 설치...]" && echo
-    yarn install
-    echo && echo  "[프론트엔드 빌드...]" && echo
-    npm run build
+    #    echo && echo  "[프론트엔드 설치...]" && echo
+    #    yarn install
+    echo && echo && echo && echo  "[프론트엔드 빌드...]" && echo
+    yarn build
     cd ..
 }
 #copy() {
@@ -30,4 +30,4 @@ setupClient() {
 #installDependencies && setupServer && setupClient && copy
 #installDependencies && setupServer && setupClient && copy
 installDependencies && setupServer && setupClient
-echo && echo && ehco  '<<<<<<<< bash 종료 >>>>>>>>>>'  &&  echo
+echo && echo && ehco  '<<<<<<<< bash 종료 >>>>>>>>>>'  &&  echo &&  echo
